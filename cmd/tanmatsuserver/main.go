20762e931a83386a6c808cb4a77d0b750aec2155
Kusubooru <kusubooru@gmail.com>
2018-09-28 02:16:33 +0300

    Implement auth mechanism with monban tokens

--- cmd/tanmatsuserver/main.go
+++ cmd/tanmatsuserver/main.go
@@ -5,9 +5,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"runtime"
 
 	"github.com/kusubooru/tanmatsu"
 	"github.com/kusubooru/tanmatsu/mysql"
+)
+
+var (
+	theVersion = "devel"
 )
 
 func main() {
@@ -18,13 +25,26 @@ func main() {
 
 func run() error {
 	var (
-		user   = flag.String("user", "eribo", "username to use to connect to MySQL")
-		pass   = flag.String("pass", "", "password to use to connect to MySQL")
-		host   = flag.String("host", "localhost", "host for connecting to MySQL")
-		port   = flag.String("port", "3306", "port for connecting to MySQL")
-		dbname = flag.String("dbname", "eribo", "database to use")
+		user        = flag.String("user", "eribo", "username to use to connect to MySQL")
+		pass        = flag.String("pass", "", "password to use to connect to MySQL")
+		host        = flag.String("host", "localhost", "host for connecting to MySQL")
+		port        = flag.String("port", "3306", "port for connecting to MySQL")
+		dbname      = flag.String("dbname", "eribo", "database to use")
+		secret      = flag.String("secret", "", "secret to decode JWT")
+		issuer      = flag.String("issuer", "monban", "accepting tokens from this issuer")
+		showVersion = flag.Bool("v", false, "print program version")
 	)
 	flag.Parse()
+
+	version := fmt.Sprintf("%s %s (runtime: %s)", filepath.Base(os.Args[0]), theVersion, runtime.Version())
+	if *showVersion {
+		fmt.Println(version)
+		return nil
+	}
+
+	if *secret == "" {
+		return fmt.Errorf("no secret provided, use -secret")
+	}
 
 	dataSource := fmt.Sprintf("%s:%s@(%s:%s)/%s?parseTime=true", *user, *pass, *host, *port, *dbname)
 	store, err := mysql.NewTanmatsuStore(dataSource)
@@ -32,7 +52,7 @@ func run() error {
 		return fmt.Errorf("creating store: %v", err)
 	}
 
-	s := tanmatsu.NewServer(store)
+	s := tanmatsu.NewServer(store, version, *issuer, []byte(*secret))
 	log.Println("listening on :8080")
 	return http.ListenAndServe(":8080", s)
 }
--- tanmatsu.go
+++ tanmatsu.go
@@ -9,31 +9,46 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
 	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
 type Image struct {
-	ID      int64
-	URL     string
-	Done    bool
-	Kuid    int
-	Created time.Time
-	Player  string
-	Channel string
+	ID      int64     `json:"id"`
+	URL     string    `json:"url"`
+	Done    bool      `json:"done"`
+	Kuid    int       `json:"kuid"`
+	Created time.Time `json:"created"`
+	Player  string    `json:"player"`
+	Channel string    `json:"channel"`
 }
 
 type imageStore interface {
 	GetImages(ctx context.Context, limit, offset int, orderAsc, isDone bool) ([]*Image, error)
 }
 
-type code string
-
 const (
-	internal code = "internal"
+	internal     = "internal"
+	unauthorized = "unauthorized"
 )
 
+var toHTTPStatus = map[string]int{
+	internal:     http.StatusInternalServerError,
+	unauthorized: http.StatusUnauthorized,
+}
+
+func HTTPStatus(c string) int {
+	status, ok := toHTTPStatus[c]
+	if !ok {
+		return http.StatusInternalServerError
+	}
+	return status
+}
+
 type Error struct {
-	Code    code
+	Code    string
 	Message string
 	Op      string
 	Err     error
@@ -60,16 +75,34 @@ func (e *Error) Error() string {
 	return buf.String()
 }
 
+// ErrorCode returns the code of the root error, if available. Otherwise returns internal.
+func ErrorCode(err error) string {
+	if err == nil {
+		return ""
+	} else if e, ok := err.(*Error); ok && e.Code != "" {
+		return e.Code
+	} else if ok && e.Err != nil {
+		return ErrorCode(e.Err)
+	}
+	return internal
+}
+
 type handler func(http.ResponseWriter, *http.Request) *Error
 
 func (fn handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if e := fn(w, r); e != nil {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		//w.WriteHeader(e.Code)
-		if e.Code == internal {
+		w.WriteHeader(HTTPStatus(ErrorCode(e)))
+		if ErrorCode(e) == internal {
 			log.Println(e.Err)
 		}
-		if err := json.NewEncoder(w).Encode(e); err != nil {
+
+		type ErrorResp struct {
+			Message string `json:"message"`
+			Err     string `json:"err"`
+		}
+		resp := ErrorResp{Message: e.Message, Err: e.Err.Error()}
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	}
@@ -78,19 +111,95 @@ func (fn handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 type server struct {
 	images   imageStore
 	handlers http.Handler
+	version  string
+	issuer   string
+	secret   []byte
 }
 
-func NewServer(imageStore imageStore) *server {
-	s := &server{images: imageStore}
+func NewServer(imageStore imageStore, version, issuer string, secret []byte) *server {
+	s := &server{
+		images:  imageStore,
+		version: version,
+		issuer:  issuer,
+		secret:  secret,
+	}
 
 	mux := http.NewServeMux()
-	mux.Handle("/images", handler(s.handleImages))
+	mux.Handle("/", handler(s.handleHome))
+	mux.Handle("/images", s.auth(s.handleImages))
 	s.handlers = mux
 	return s
 }
 
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.handlers.ServeHTTP(w, r)
+}
+
+func (s *server) handleHome(w http.ResponseWriter, r *http.Request) *Error {
+	fmt.Fprintln(w, s.version)
+	return nil
+}
+
+func (s *server) auth(h handler) handler {
+	var op = "auth"
+	var msg = "Authorization failed."
+	return func(w http.ResponseWriter, r *http.Request) *Error {
+		token, err := getTokenFromHeader(r.Header)
+		if err != nil {
+			return &Error{Code: unauthorized, Op: op, Message: msg, Err: err}
+		}
+
+		if err := validateToken(token, s.secret, s.issuer); err != nil {
+			return &Error{Code: unauthorized, Op: op, Message: msg, Err: err}
+		}
+
+		return h(w, r)
+	}
+}
+
+func getTokenFromHeader(header map[string][]string) (string, error) {
+	var tokenString string
+	tokens, ok := header["Authorization"]
+	if !ok || len(tokens) < 1 {
+		return "", fmt.Errorf("expecting Authorization header")
+	}
+
+	tokenString = tokens[0]
+	if !strings.HasPrefix(tokenString, "Bearer ") {
+		return "", fmt.Errorf("expecting Bearer auth type")
+	}
+	tokenString = strings.TrimPrefix(tokenString, "Bearer ")
+
+	return tokenString, nil
+}
+
+func validateToken(tokenString string, secret []byte, issuer string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return secret, nil
+	})
+	if err != nil {
+		return fmt.Errorf("parsing token: %v", err)
+	}
+
+	if !token.Valid {
+		return fmt.Errorf("invalid token")
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return fmt.Errorf("expecting MapClaims")
+	}
+	iss, ok := claims["iss"]
+	if !ok {
+		return fmt.Errorf("no issuer defined in claims")
+	}
+	if iss != issuer {
+		return fmt.Errorf("unknown issuer")
+	}
+	return nil
 }
 
 func (s *server) handleImages(w http.ResponseWriter, r *http.Request) *Error {
